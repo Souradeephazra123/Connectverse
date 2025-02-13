564449e10b0aa5270ffae730ad49dfe588d321ab
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2025-02-14 02:06:20 +0530

    added frontend url in server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: ["http://localhost:3000", "http://localhost:3001",`${process.env.FRONTEND_URL}`],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
