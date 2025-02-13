9042dd43afc287182d65f84ef3ac774f541b0c8b
SOURADEEP HAZRA <105676331+Souradeephazra123@users.noreply.github.com>
2025-02-14 01:59:00 +0530

    added authorization

--- src/routes/event.routes.js
+++ src/routes/event.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import {  getAllSongs } from '../controller/event.controller.js';
+import { authenticate } from '../controller/auth.middleware.js';
 
 const EventRouter = express.Router();
 
 
-EventRouter.get('/songs', getAllSongs);
+EventRouter.get('/songs',authenticate, getAllSongs);
 
 export default EventRouter;
